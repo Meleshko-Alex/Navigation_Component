77daf066132f73b0548c536cb2de9bfec85a0b1a
Alex Meleshko <meleha.alex@gmail.com>
2024-11-28 10:00:45 +0200

    Add view models to screens

--- app/src/main/java/com/meleha/navcomponent/ui/screens/items/ItemsScreen.kt
+++ app/src/main/java/com/meleha/navcomponent/ui/screens/items/ItemsScreen.kt
@@ -3,39 +3,62 @@ package com.meleha.navcomponent.ui.screens.items
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.meleha.navcomponent.ui.screens.AddItemRoute
 import com.meleha.navcomponent.ui.screens.LocalNavController
+import com.meleha.navcomponent.ui.screens.items.ItemsViewModel.*
 
 @Composable
 fun ItemsScreen() {
+    val viewModel: ItemsViewModel = hiltViewModel()
     val navController = LocalNavController.current
+    val screenState = viewModel.stateFlow.collectAsState()
     ItemsContent(
+        getScreenState = { screenState.value },
         onLaunchAddItemScreen = { navController.navigate(AddItemRoute) }
     )
 }
 
 @Composable
-fun ItemsContent(onLaunchAddItemScreen: () -> Unit) {
+fun ItemsContent(
+    getScreenState: () -> ScreenState,
+    onLaunchAddItemScreen: () -> Unit,
+) {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Items screen",
-            modifier = Modifier
-                .align(Alignment.Center),
-            fontSize = 20.sp,
-        )
+        when (val screenState = getScreenState()) {
+            ScreenState.Loading -> {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+
+            is ScreenState.Success -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(screenState.items) {
+                        Text(
+                            text = it,
+                            modifier = Modifier.padding(12.dp),
+                        )
+                    }
+                }
+            }
+        }
         FloatingActionButton(
             onClick = onLaunchAddItemScreen,
             modifier = Modifier
@@ -50,5 +73,8 @@ fun ItemsContent(onLaunchAddItemScreen: () -> Unit) {
 @Preview(showSystemUi = true)
 @Composable
 private fun ItemsPreview() {
-    ItemsContent(onLaunchAddItemScreen = {})
+    ItemsContent(
+        getScreenState = { ScreenState.Loading },
+        onLaunchAddItemScreen = {}
+    )
 }
